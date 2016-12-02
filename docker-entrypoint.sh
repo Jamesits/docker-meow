faed6b7e494ae0c30262f0ce7aa9ecf9c99a09b3
James Swineson <github@public.swineson.me>
2016-12-01 19:30:00 -0600

    remove additional spaces

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ if [[ "$1" == *MEOW ]]; then
     curl "$CONFIG_URL" --output $HOME/.meow/rc
   elif [ -n "$PROXY" ]; then
     # easy config
-	  printf "listen = http://0.0.0.0:4411\njudgeByIP = true\nproxy = $PROXY\n" > $HOME/.meow/rc
+    printf "listen = http://0.0.0.0:4411\njudgeByIP = true\nproxy = $PROXY\n" > $HOME/.meow/rc
   else
     echo "No proxy settings found in environment variables. You may need to set CONFIG_URL or PROXY."
   fi
