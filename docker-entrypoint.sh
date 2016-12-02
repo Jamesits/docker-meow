3e0ac2a6846fee69b37339876a6395ee4e9099dc
James Swineson <github@public.swineson.me>
2016-12-01 19:26:42 -0600

    Option to download a file as configuration

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,14 @@
 set -e
 
 if [[ "$1" == *MEOW ]]; then
-  printf "listen = http://0.0.0.0:4411\njudgeByIP = true\nproxy = $PROXY\n" > ~/.meow/rc
+  if [ -n "$CONFIG_URL" ]; then
+    curl "$CONFIG_URL" --output $HOME/.meow/rc
+  elif [ -n "$PROXY" ]; then
+    # easy config
+	  printf "listen = http://0.0.0.0:4411\njudgeByIP = true\nproxy = $PROXY\n" > $HOME/.meow/rc
+  else
+    echo "No proxy settings found in environment variables. You may need to set CONFIG_URL or PROXY."
+  fi
 fi
 
 exec "$@"
